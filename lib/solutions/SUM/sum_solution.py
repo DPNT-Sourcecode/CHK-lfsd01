bae8689055b18ee98c3be08877f556837d61c30e
sbx_user1051 <sbx_user1051@169.254.152.25>
2022-06-27 10:50:09 +0000

    Mon Jun 27 10:50:09 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,22 +1,32 @@
 
 def _checks(x, y):
+    """Internal checks for the compute function.
 
+    Args:
+        x (int): input param1
+        y (int): input param2
+
+    Raises:
+        TypeError: if x or y are not int.
+        ValueError: if x or y are larger than 100
+    """
     if not isinstance(x, int):
         raise TypeError(f"x must be integer, got {type(x)}")
         
     if not isinstance(y, int):
         raise TypeError(f"y must be integer, got {type(x)}")
 
-    if not x < 100:
-        raise ValueError("x must be smaller than 100, got {x}")
+    if not 0 <= x <= 100:
+        raise ValueError("x must be between 0 and 100 (bounds included), got {x}")
 
-    if not y < 100:
-        raise ValueError("y must be smaller than 100, got {x}")
+    if not 0 <= y <= 100:
+        raise ValueError("y must be between 0 and 100 (bounds included), got {x}")
 
 def compute(x: int, y: int) -> int:
 
     _checks(x, y)
     
     return x + y
+
 
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,20 +7,36 @@ class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
-    def test_sum_input_type(self):
+    def test_sum_input(self):
         """Check that the function raises a type exception"""
         with pytest.raises(TypeError):
             sum_solution.compute('a', 1)
+
         with pytest.raises(TypeError):    
             sum_solution.compute(2, 'a')
+
         with pytest.raises(TypeError):
             sum_solution.compute('a', 'b')
+
         with pytest.raises(TypeError):
             sum_solution.compute(3.2, 0.8)
+
+        with pytest.raises(ValueError):
+            sum_solution.compute(101, 3)
+
+        with pytest.raises(ValueError):
+            sum_solution.compute(3, 101)
+
+        with pytest.raises(ValueError):
+            sum_solution.compute(-1, 3)
+
+        with pytest.raises(ValueError):
+            sum_solution.compute(3, -1)
 
     def test_sum_output_type(self):
         """Check that the function raises a type exception"""
         assert isinstance(sum_solution.compute(2, 1), int)
         
         
+
 
