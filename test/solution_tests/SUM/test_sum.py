384e345f9c93f40559e4e4d485a07ac31eb15dda
sbx_user1051 <sbx_user1051@169.254.152.25>
2022-06-27 10:56:09 +0000

    Mon Jun 27 10:56:09 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -9,29 +9,46 @@ class TestSum():
 
     def test_sum_input(self):
         """Check that the input fulfils requirements"""
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError) as exc:
             sum_solution.compute('a', 1)
+        
+        assert f"x must be integer, got {type('a')}" == str(exc.value)
 
-        with pytest.raises(TypeError):    
+        with pytest.raises(TypeError) as exc:   
             sum_solution.compute(2, 'a')
+        
+        assert f"y must be integer, got {type('a')}" == str(exc.value)
 
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError) as exc:
             sum_solution.compute('a', 'b')
+
+        assert f"x must be integer, got {type('a')}" == str(exc.value)
 
         with pytest.raises(TypeError):
             sum_solution.compute(3.2, 0.8)
 
+        assert f"x must be integer, got {type(3.2)}" == str(exc.value)
+
         with pytest.raises(ValueError):
             sum_solution.compute(101, 3)
+
+        assert f"x must be between 0 and 100 (bounds included), got 101" == str(exc.value)
 
         with pytest.raises(ValueError):
             sum_solution.compute(3, 101)
 
+        assert f"y must be between 0 and 100 (bounds included), got 101" == str(exc.value)
+
         with pytest.raises(ValueError):
             sum_solution.compute(-1, 3)
 
+        assert f"x must be between 0 and 100 (bounds included), got 101" == str(exc.value)
+
         with pytest.raises(ValueError):
             sum_solution.compute(3, -1)
+
+        assert f"x must be between 0 and 100 (bounds included), got 101" == str(exc.value)
+
 
     def test_sum_output_type(self):
         """Check that the function returns an int."""
