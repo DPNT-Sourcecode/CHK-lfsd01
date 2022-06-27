c09e5c3dcba281df6a4cc7a69901bc730d669262
sbx_user1051 <sbx_user1051@169.254.152.25>
2022-06-27 10:41:09 +0000

    Mon Jun 27 10:41:09 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,22 @@
 
+def _checks(x, y):
+
+    if not isinstance(x, int):
+        raise TypeError(f"x must be integer, got {type(x)}")
+        
+    if not isinstance(y, int):
+        raise TypeError(f"y must be integer, got {type(x)}")
+
+    if not x < 100:
+        raise ValueError("x must be smaller than 100, got {x}")
+
+    if not y < 100:
+        raise ValueError("y must be smaller than 100, got {x}")
 
 def compute(x: int, y: int) -> int:
 
+    _checks(x, y)
     
     return x + y
+
 
