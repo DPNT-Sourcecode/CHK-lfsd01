c461cd0f70acf8bf2ad8e975f076f54863032c2a
sbx_user1051 <sbx_user1051@169.254.152.25>
2022-06-27 10:44:09 +0000

    Mon Jun 27 10:44:09 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,16 @@
+from typing import Type
+import pytest
+
 from solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_input_type(self):
+        with pytest.raises(TypeError):
+            sum_solution.compute('a', 1)
+            sum_solution.compute(2, 'a')
+            sum_solution.compute('a', 'b')
+
