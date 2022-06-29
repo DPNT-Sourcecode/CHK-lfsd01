22129d0ecc76b42c8d9af1f0ba8bb5d47f68c3d7
sbx_user1051 <sbx_user1051@169.254.26.125>
2022-06-29 08:49:20 +0000

    Wed Jun 29 08:49:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,31 @@
 
 
-# noinspection PyUnusedLocal
-# skus = unicode string
+SKUS_PRICES = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15
+}
+
+SKUS_OFFERS = {
+    'A': {'qty': 3, 'price': 130},
+    'B': {'qty': 2, 'price': 45},
+}
+
+def apply_offers():
+
+    # noinspection PyUnusedLocal
+    # skus = unicode string
+
+
 def checkout(skus: str) -> int:
-    
-    
+
+    items_in_skus = set(skus)
+
+    counts = {item: skus.count(item) for item in items_in_skus}
+
+    tot_checkout = []
+
     return tot_checkout
+
 
