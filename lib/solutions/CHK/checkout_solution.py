a033aa7c12742f1c8de11b27055b011d0182d106
sbx_user1051 <sbx_user1051@169.254.162.137>
2022-06-29 10:20:14 +0000

    Wed Jun 29 10:20:14 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,12 +6,36 @@ OFFERS = {
     'C': [{'qty': 1, 'price': 20}],
     'D': [{'qty': 1, 'price': 15}],
     'E': [{'qty': 1, 'price': 40}],
-    'F': [{'qty': 1, 'price': 10}]
+    'F': [{'qty': 1, 'price': 10}],
+    'G': [{'qty': 1, 'price': 20}],
+    'H': [{'qty': 10, 'price': 80}, {'qty': 5, 'price': 45}, {'qty': 1, 'price': 10}],
+    'I': [{'qty': 1, 'price': 35}],
+    'J': [{'qty': 1, 'price': 60}],
+    'K': [{'qty': 2, 'price': 150}, {'qty': 1, 'price': 80}],
+    'L': [{'qty': 1, 'price': 90}],
+    'M': [{'qty': 1, 'price': 15}],
+    'N': [{'qty': 1, 'price': 40}],
+    'O': [{'qty': 1, 'price': 10}],
+    'P': [{'qty': 5, 'price': 200}, {'qty': 1, 'price': 50}],
+    'Q': [{'qty': 3, 'price': 80}, {'qty': 1, 'price': 30}],
+    'R': [{'qty': 1, 'price': 50}],
+    'S': [{'qty': 1, 'price': 30}],
+    'T': [{'qty': 1, 'price': 20}],
+    'U': [{'qty': 1, 'price': 40}],
+    'V': [{'qty': 3, 'price': 130}, {'qty': 2, 'price': 90}, {'qty': 1, 'price': 50}],
+    'W': [{'qty': 1, 'price': 20}],
+    'X': [{'qty': 1, 'price': 90}],
+    'Y': [{'qty': 1, 'price': 10}],
+    'Z': [{'qty': 1, 'price': 50}],
 }
+
 
 REPLACEMENT_OFFERS = {
     'E': {'qty': 2, 'replace_with': 'B', 'replace_qty': 1},
-    'F': {'qty': 3, 'replace_with': 'F', 'replace_qty': 1}
+    'F': {'qty': 3, 'replace_with': 'F', 'replace_qty': 1},
+    'N': {'qty': 3, 'replace_with': 'M', 'replace_qty': 1},
+    'R': {'qty': 3, 'replace_with': 'Q', 'replace_qty': 1},
+    'U': {'qty': 3, 'replace_with': 'U', 'replace_qty': 1},
 }
 
 
@@ -77,4 +101,5 @@ def checkout(skus: str) -> int:
     ) for sku_name, count in counts.items()])
 
     return tot_checkout
+
 
