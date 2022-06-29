8c1223ed0b740cf413fb7bb8bff77331b27b9fb7
sbx_user1051 <sbx_user1051@169.254.26.125>
2022-06-29 09:01:20 +0000

    Wed Jun 29 09:01:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,24 +7,21 @@ PRICES = {
     'D': 15
 }
 
-OFFERS_QTY = {
-    'A': 3,
-    'B': 2,
-}
-
-OFFERS_PRICES = {
-    'A': 130,
-    'B': 45,
+OFFERS = {
+    'A': {'qty': 3, 'price': 130},
+    'B': {'qty': 2, 'price': 45},
 }
 
 
-def compute_price(sku_name, count, full_price, offer_qty, offer_price):
+def compute_price(count, full_price, offer):
 
-    # full_price = SKUS_PRICES[sku_name]
-    # offer_qty = SKUS_PRICES[sku_name]['qty']
-    # offer_price = SKUS_PRICES[sku_name]['price']
+    if offer:
+        offer_qty = offer['qty']
+        offer_price = offer['price']
+        tot_price = full_price * (count % offer_qty) + offer_price * (count // offer_qty)
 
-    tot_price = full_price * (count % offer_qty) + offer_price * (count // offer_qty)
+    else:
+        tot_price = full_price * count
 
     return tot_price
 
@@ -36,12 +33,13 @@ def checkout(skus: str) -> int:
     counts = {item: skus.count(item) for item in items_in_skus}
 
     tot_checkout = [compute_price(
-        sku_name,
         count,
         PRICES.get(sku_name, 0),
-        OFFERS.get(sku_name, 0)) for sku_name, count in counts.items()]
+        OFFERS.get(sku_name, None)
+    ) for sku_name, count in counts.items()]
 
     return tot_checkout
+
 
 
 
---
+++ test/solution_tests/CHK_R1/test_hello.py
@@ -0,0 +1,26 @@
+
+import pytest
+from solutions.CHK import checkout_solution
+
+
+class TestHello():
+    """pytest class for testing the Hello solution"""
+
+    def test_sum(self):
+        assert hello_solution.hello('iwoka') == 'Hello, iwoka!'
+
+    def test_hello_input(self):
+        """Check that the input fulfils requirements"""
+        with pytest.raises(TypeError) as exc:
+            hello_solution.hello(20)
+
+        assert f"friend_name must be a string, got {type(20)}" == str(exc.value)
+
+        with pytest.raises(TypeError) as exc:
+            hello_solution.hello(10.)
+
+        assert f"friend_name must be a string, got {type(10.)}" == str(exc.value)
+
+    def test_hello_output_type(self):
+        """Check that the function returns an int."""
+        assert isinstance(hello_solution.hello('Marco'), str)
